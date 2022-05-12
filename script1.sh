fec9e9babba0ab4cd9994d9008bf112cc2e45dbd
AdelKarimBilalYasinAwanMalik <86436710+AdelKarimBilalYasinAwanMalik@users.noreply.github.com>
2022-05-13 00:27:42 +0200

    script main 2

--- script1.sh
+++ script1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "start der installation"
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -6,6 +7,7 @@ sudo apt-get install -y \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
+echo "zwischenstand 1"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -14,6 +16,8 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo groupadd docker
 sudo usermod -aG docker $USER
+echo "zwischenstand 2"
 sudo systemctl enable docker
 sudo docker run -d --name prometheus-container -e TZ=CET -p 30090:9090 ubuntu/prometheus:2.33-22.04_beta
 sudo docker run -d --name grafana-container -e TZ=CET -p 3000:3000 grafana/grafana-oss
+echo "Container laufen jetzt"
