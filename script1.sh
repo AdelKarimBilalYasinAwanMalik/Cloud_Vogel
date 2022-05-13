353ce4c2c07abf85a03ce426b76b3a24e41a56a2
AdelKarimBilalYasinAwanMalik <86436710+AdelKarimBilalYasinAwanMalik@users.noreply.github.com>
2022-05-13 02:12:55 +0200

    script main 4

--- script1.sh
+++ script1.sh
@@ -19,7 +19,7 @@ sudo usermod -aG docker $USER
 echo "zwischenstand 2"
 sudo systemctl enable docker
 
-cat <<'EOF'>/home/dockerroot/prometheus.yml
+cat <<'EOF'>/home/Saiyajin/prometheus.yml
 global:
   scrape_interval:     5s
   evaluation_interval: 5s
@@ -38,7 +38,7 @@ scrape_configs:
       - targets: ['192.168.66.183:9100']
 EOF
 
-sudo docker run -d -p 9090:9090 -v /home/dockerroot/prometheus.yml:/etc/prometheus/prometheus.yml --name=prometheus prom/prometheus
+sudo docker run -d -p 9090:9090 -v /home/Saiyajin/prometheus.yml:/etc/prometheus/prometheus.yml --name=prometheus prom/prometheus
 
 sudo docker run -d --name grafana-container -e TZ=CET -p 3000:3000 grafana/grafana-oss
 echo "Container laufen jetzt"
